ffdf3a6c5fa4c365ff85f0c0745c35053323f27c
gongyoon93 <qkszn93@gmail.com>
2024-11-02 21:10:24 +0900

    default url setting

--- src/app/abook/page.tsx
+++ src/app/abook/page.tsx
@@ -144,4 +144,15 @@ const Abook = () => {
   );
 };
 
+export async function getStaticProps() {
+  // const res = await fetch('https://.../posts')
+  // const posts = await res.json()
+
+  return {
+    props: {
+      // posts,
+    },
+  };
+}
+
 export default Abook;
